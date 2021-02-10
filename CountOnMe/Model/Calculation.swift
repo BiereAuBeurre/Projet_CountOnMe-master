f4e909cf966c778d00b069c99f878605c2d2b3d9
Manon Russo <russo.manon@gmail.com>
2021-02-10 15:48:17 +0100

    addition and soustraction test are ok not refactored yet

---
+++ CountOnMe/Model/Calculation.swift
@@ -0,0 +1,23 @@
+//
+//  Calculation.swift
+//  CountOnMe
+//
+//  Created by Manon Russo on 10/02/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calculation {
+    
+    func addition(firstNumber: Int, secondNumber: Int) -> String {
+        let result = firstNumber + secondNumber
+        return "\(result)"
+    }
+    
+    func soustraction(firstNumber: Int, secondNumber: Int) -> String {
+    let result = firstNumber - secondNumber
+    return "\(result)"
+    }
+}
+
--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -6,29 +6,35 @@
 //  Copyright © 2019 Vincent Saluzzo. All rights reserved.
 //
 
-//import XCTest
-//@testable import SimpleCalc
-//
-//class SimpleCalcTests: XCTestCase {
-//
-//    override func setUp() {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-//
-//}
+import XCTest
+@testable import CountOnMe
+
+class SimpleCalcTests: XCTestCase {
+    var calculation: Calculation?
+    override func setUp() {
+        calculation = Calculation()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    override func tearDown() {
+        calculation = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAddition() {
+        // Given
+        let a = 3
+        let b = 4
+        // When
+        let result = calculation?.addition(firstNumber: a, secondNumber: b)
+        // Then
+        XCTAssertEqual(result, "7")
+    }
+    
+    func testSoustraction() {
+        let firstNumber = 20
+        let secondNumber = 15
+        let result = calculation?.soustraction(firstNumber: firstNumber, secondNumber: secondNumber)
+        XCTAssertEqual(result, "5")
+    }
+
+}
